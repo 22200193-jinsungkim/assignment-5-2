6ccbae6c8309607ee551eed95aab03282401d42a
22200193-jinsungkim <22200193@handong.ac.kr>
2025-11-29 19:43:35 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useState } from "react";
+import myData from "./my_data.json";
+
 import List from "./pages/List";
 import Detail from "./pages/Detail";
 import Update from "./pages/Update";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
+
+  // 🔥 my_data.json의 GAME 배열을 state로 관리
+  const [list, setList] = useState(myData.GAME);
+
   return (
     <BrowserRouter>
       <nav className="navbar navbar-dark bg-dark">
         <div className="container d-flex justify-content-center">
-
           <ul className="navbar-nav d-flex flex-row justify-content-center gap-4">
-
             <li className="nav-item">
               <Link className="nav-link" to="/List">LIST</Link>
             </li>
@@ -24,15 +30,16 @@ function App() {
             <li className="nav-item">
               <Link className="nav-link" to="/Update">EDIT</Link>
             </li>
-
           </ul>
         </div>
       </nav>
+
       <Routes>
-        <Route path="List" element={<List />} />
-        <Route path="Detail" element={<Detail />} />
-        <Route path="Update" element={<Update />} />
+        <Route path="/List" element={<List list={list} />} />
+        <Route path="/Detail" element={<Detail />} />
+        <Route path="/Update" element={<Update list={list} setList={setList} />} />
       </Routes>
+
     </BrowserRouter>
   );
 }
